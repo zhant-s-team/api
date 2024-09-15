5985d2e3a7543dadf17a7e6798c6c0041fdfdaa7
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-09-15 16:09:05 -0300

    feat: :seedling: Adicionando Controller de Usuario.

--- ProjetoApi/app/Http/Controllers/UsuarioController.php
+++ ProjetoApi/app/Http/Controllers/UsuarioController.php
@@ -12,54 +12,44 @@ class UsuarioController extends Controller
      */
     public function index()
     {
-
+        return Usuario::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
+        $validatedData = $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|unique:usuarios',
+            'senha' => 'required|string|min:8',
+        ]);
 
+        $usuario = Usuario::create($validatedData);
+        return response()->json($usuario, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-
+        return Usuario::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
+        $usuario = Usuario::findOrFail($id);
+        $validatedData = $request->validate([
+            'nome' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|email|unique:usuarios,email,' . $id,
+            'senha' => 'sometimes|required|string|min:8',
+            // Outros campos do modelo Usuario
+        ]);
 
+        // Atualiza o usuário com os dados validados
+        $usuario->update($validatedData);
+        return response()->json($usuario, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-
+        Usuario::destroy($id);
+        return response()->json(null, 204);
     }
 }
