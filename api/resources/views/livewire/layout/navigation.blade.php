0b32e6366c0a7417430a289dcf39249690446104
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-11-04 02:28:59 -0300

    feat: :seedling: Links dos menus

--- api/resources/views/livewire/layout/navigation.blade.php
+++ api/resources/views/livewire/layout/navigation.blade.php
@@ -36,10 +36,12 @@ new class extends Component
                     <x-nav-link :href="route('entregas')" :active="request()->routeIs('entregas')" wire:navigate>
                         {{ __('Entregas') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('empresas')" :active="request()->routeIs('empresas')" wire:navigate>
+                        {{ __('Empresas') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')" wire:navigate>
                         {{ __('Usuários') }}
                     </x-nav-link>
-
                 </div>
             </div>
 
@@ -75,10 +77,10 @@ new class extends Component
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = !open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -86,14 +88,19 @@ new class extends Component
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': !open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-
             <x-responsive-nav-link :href="route('entregas')" :active="request()->routeIs('entregas')" wire:navigate>
                 {{ __('Entregas') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('empresas')" :active="request()->routeIs('empresas')" wire:navigate>
+                {{ __('Empresas') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')" wire:navigate>
+                {{ __('Usuários') }}
             </x-responsive-nav-link>
         </div>
 
