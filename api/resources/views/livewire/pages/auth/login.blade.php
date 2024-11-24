203b125b384fbb8700f1eda9108ea73de3c18e7c
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-11-23 23:51:28 -0300

    style: :art: Estilizando pagina inicial e de login

--- api/resources/views/livewire/pages/auth/login.blade.php
+++ api/resources/views/livewire/pages/auth/login.blade.php
@@ -24,7 +24,7 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
+<div class="dark">
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -11,55 +11,49 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
-            <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:justify-center lg:col-start-2">
-                            VIACargo
-                        </div>
+        <section class="bg-gray-900 text-white">
+            <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+                <div class="mx-auto max-w-3xl text-center">
+                    <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5x5">
+                        ViaCargo
+                    </h1>
+
+                    <div class="mt-8 flex flex-wrap justify-center gap-4">
                         @if (Route::has('login'))
-                            <nav class="-mx-3 flex flex-1 justify-end">
-                                @auth
-                                    <a
-                                        href="{{ url('/dashboard') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Voltar ao Dashboard
-                                    </a>
-                                @else
-                                    <a
-                                        href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Log in
-                                    </a>
+                            @auth
+                                <a
+                                    href="{{ url('/dashboard') }}"
+                                    class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                                >
+                                    Voltar ao Dashboard
+                                </a>
+                            @else
+                                <a
+                                    href="{{ route('login') }}"
+                                    class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                                >
+                                    Log in
+                                </a>
 
-                                    @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Criar Conta
-                                        </a>
-                                    @endif
-                                @endauth
-                            </nav>
+                                @if (Route::has('register'))
+                                    <a
+                                        href="{{ route('register') }}"
+                                        class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+                                    >
+                                        Criar Conta
+                                    </a>
+                                @endif
+                            @endauth
                         @endif
-                    </header>
-
-                    <main class="mt-6">
-                    </main>
-
-                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        Desenvolvido por Zhant interprise
-                    </footer>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+
+        <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+            Desenvolvido por Zhant interprise
+        </footer>
     </body>
 </html>
